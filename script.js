14ab6f09c326ec62c5d34262443ae58c269eef91
ErneC <mv.ernestina@gmail.com>
2022-09-27 21:45:10 -0300

    primeiro exercicio js

---
+++ script.js
@@ -0,0 +1,8 @@
+
+console.log(document.getElementById("divisao2").innerHTML = "Oi");
+//innerHTML acessa o texto.
+// innerHTML = "Oi" vai modificar o texto original pelo "oi."
+
+console.log(document.getElementsByClassName("classe1")[0]);
+
+//o zero dentro do colchete informa que ele quer acessar o conteudo do primeiro div/classe1.
